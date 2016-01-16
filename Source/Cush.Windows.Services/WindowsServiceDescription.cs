a7d429cad1bb7e4b611d9acdbe53be8313a61eba
Curt Kaler <lionfree@earthlink.net>
2016-01-16 12:20:55 -0800

    Start updating WindowsServiceDescription to allow for multiple endpoints.

---
+++ Source/Cush.Windows.Services/WindowsServiceDescription.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ServiceModel.Channels;
+
+namespace Cush.Windows.Services
+{
+    public class WindowsServiceDescription
+    {
+        
+        public object ServiceObject;
+        public Type ServiceType;
+        public Endpoint[] Endpoints;
+    }
+
+    public class Endpoint
+    {
+        public object ServiceObject;
+        public Type ServiceType;
+
+        public Binding Binding;
+        public string Path;
+        public Type Contract;
+        public int Port = -1;
+    }
+}
